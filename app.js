3324edda7a0377c47b30bd857d0f48b29bfb1d99
AbdeljalilElhassani <90702628+AbdeljalilElhassani@users.noreply.github.com>
2022-08-03 21:46:23 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var Dark = 0;
+function myFunction(){
+    var container = document.getElementById("container");
+    var logo = document.getElementById("logo");
+    container.classList.toggle("backgroundBlack")
+    logo.classList.toggle("logoDark");
+    
+
+
+    
+    
+}
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fatima's Project</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="container" id="container">
+
+        <button onclick="myFunction()" class="darkModeButton">
+
+        </button>
+        <div class="logo" id="logo">
+
+        </div>
+        <form method="get" action="" >
+            <img src="./images/LoginIcon.png" alt="">
+            <h1>Login</h1>
+            <input type="text" placeholder="Login">
+            <input type="password" placeholder="Password" >
+            <input type="submit" value="Login" class="loginBtn">
+        </form>
+
+
+    </div>
+    <script src="./app.js"></script>
+</body>
+</html>
